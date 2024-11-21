28a5b51c938bd1453a03ffabc91dde48ed99f63b
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2024-11-21 22:40:35 +0000

    Initial deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://robin.trulyao.dev",
 	integrations: [
 		starlight({
 			title: "Robin",
